9538d3b03442ab209c40d56df600be218ecac404
ADARSH29112003 <solankiadarshsingh29112003@gmail.com>
2026-07-23 21:14:53 +0530

    Learn Methods and Parameters in java
    What I Learned
    
    - Introduction to Methods (Functions) in Java
    - Why methods are used in real-world programming
    - Method creation and method calling
    - Reusable code concept
    - Method Parameters
    - Multiple Parameters
    - Basic understanding of Parameters vs Arguments
    - Method execution flow
    
    Programs Created
    
    1. GreetingMethod.java
       - Created first custom method
       - Called method from main()
    
    2. AdditionMethod.java
       - Used multiple parameters
       - Performed addition using method inputs
    
    Interview Preparation
    
    - What is a Method in Java?
    - Why do we use Methods?
    - What is a Parameter in Java?
    - Basic introduction to Parameters vs Arguments
    
    Mistakes I Made
    
    - Confused main() method with String
    - Mixed Parameter and Method concepts
    - Needed more clarity on execution flow
    - Still not fully confident about Parameters vs Arguments
    
    How I Fixed Them
    
    - Used real-life examples (Remote, Juice Machine)
    - Traced method execution step-by-step
    - Practiced method calls manually
    - Built actual programs instead of only reading theory
    
    Current Status
    
    - Lesson 1: What is a Method? ✅
    - Lesson 2: Method Parameters ✅
    - Lesson 3: Method Arguments ⏳ (Next Session)
    
    Next Target
    
    - Understand Parameters vs Arguments clearly
    - Build ParameterDemo.java
    - Learn Method Arguments
    - Continue Day 09 Methods Journey

---
+++ AdditionMethod.java
@@ -0,0 +1,34 @@
+// Requirements:
+
+// Method Name:
+// add
+
+// Parameters:
+// int a
+// int b
+
+// Print:
+// Sum = a + b
+
+// Call:
+
+// add(10,20);
+
+// Output:
+
+// Sum: 30
+
+public class AdditionMethod 
+{
+    public static void add(int a , int b)
+    {
+        int Sum = a + b;
+        System.out.println("Sum: " + Sum);
+
+    }
+
+    public static void main(String[] args)
+    {
+        add(10, 20);
+    }
+}
---
+++ GreetingMethod.java
@@ -0,0 +1,28 @@
+// Requirements:
+
+// Method Name:
+// greet()
+
+// Task:
+// Print Welcome Adarsh
+
+// Call greet() inside main()
+
+// Output:
+
+// Welcome Adarsh
+
+public class GreetingMethod
+{
+    public static void greet()
+    {
+        System.out.println("Welcome Adarsh");
+    }
+
+    public static void main(String[] args) 
+    {
+        {
+            greet();
+        }
+    }
+}
